44ef7522be5ee7d7e8ce7820005aaaf83ad764b0
yuhwanwoo <6514687@naver.com>
2020-06-17 12:19:18 +0900

    django page

--- mysite/articles/urls.py
+++ mysite/articles/urls.py
@@ -4,8 +4,12 @@ from . import views
 app_name="articles"
 
 urlpatterns=[
-    path('',views.index, name="index"),
+    path('index/',views.index, name="index"),
     path('new/',views.new,name="new"),
     path('create/',views.create,name="create"),
     path('introduce/',views.introduce,name="introduce"),
+    path('<int:article_pk>/',views.detail,name="detail"),
+    path('<int:article_pk>/delete/',views.delete,name="delete"),
+    path('<int:article_pk>/edit/',views.edit,name="edit"),
+    path('<int:article_pk>/update',views.update,name="update")
 ]
--- mysite/articles/views.py
+++ mysite/articles/views.py
@@ -1,20 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+import requests
+#모델 가져오기
+from articles.models import Article
 
 # Create your views here.
+
+# 전체 데이터 가져오기
+# 그 데이터 템플릿에게 넘겨주기
+# 템플릿에서 반복문으로 각각의 게시글 pk, title 보여주기
 def index(request):
-    return render(request,'articles/index.html')
+    article=Article.objects.all()
+    context={
+        'articles':article
+    }
+    return render(request,'articles/index.html',context)
 
 def new(request):
     return render(request,'articles/new.html')
 
 def create(request):
-    title=request.GET.get('title')
-    content=request.GET.get('content')
-    context={
-        'title' :title,
-        'content':content
-    }
-    return render(request,'articles/create.html',context)
+    title=request.POST.get('title')
+    text=request.POST.get('text')
+    Article.objects.create(title=title,content=text)
+    return redirect('articles:index')
+
 
 # 1. /introduce/
 # 2. h1 태그로 이루어진 제목
@@ -22,6 +31,34 @@ def create(request):
 # 3. back링크로 index로 돌아갈 수 있는 링크 하나
 # 4. index에서 introduce 이동할 수 있는 링크 하나
 # 5. base.html 상속받아서 block body 안에 작성
-
 def introduce(request):
     return render(request,'articles/introduce.html')
+
+def detail(request,article_pk):
+    #request.method
+    article=Article.objects.get(pk=article_pk)
+    if request.method=="POST":
+        article.delete()
+        return redirect('articles:index')
+    return redirect('articles:detail',article.pk)
+
+def delete(request,article_pk):
+    article=Article.objects.get(pk=article_pk)
+    article.delete()
+    return redirect('articles:index')
+
+def edit(request,article_pk):
+    article=Article.objects.get(pk=article_pk)
+    context={
+        'article':article
+    }
+    return render(request,'articles/edit.html',context)
+
+def update(request,article_pk):
+    edit_title=request.POST.get('edit_title')
+    edit_content=request.POST.get('edit_content')
+    article=Article.objects.get(pk=article_pk)
+    article.title=edit_title
+    article.content=edit_content
+    article.save()
+    return redirect('articles:detail',article_pk)
